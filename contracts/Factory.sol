8bf6fd93d8c149f923d59388cf73222177a6d51c
Breech Alcres Paul Oracion <oracionbre@gmail.com>
2021-09-09 20:51:33 +0800

    chore: added factory deploy pattern

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -8,27 +8,29 @@ import "hardhat/console.sol";
 contract Factory {
     OppaTwo public token;
 
-    constructor() public payable {
-        token = new OppaTwo();
+    constructor() public {
+        token = OppaTwo(0x79419dC8c39d9ECa79025Fd2520A71b13f50E390);
+
+        console.log(address(token));
 
         IPancakeRouter02 _pancakeV2Router = IPancakeRouter02(
-            0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9
+            0x9Ac64Cc6e4415144C455BD8E4837Fea55603e5c3
         );
 
         token.approve(address(_pancakeV2Router), token.balanceOf(address(1)));
 
-        (, , uint256 liquidity) = _pancakeV2Router.addLiquidityETH{
-            value: 200000000000
-        }(
-            address(token),
-            token.totalSupply(),
-            0,
-            0,
-            address(this),
-            block.timestamp
-        );
+        // (, , uint256 liquidity) = _pancakeV2Router.addLiquidityETH{
+        //     value: 200000000000
+        // }(
+        //     address(token),
+        //     token.totalSupply(),
+        //     0,
+        //     0,
+        //     address(this),
+        //     block.timestamp
+        // );
 
-        console.log(liquidity);
+        // console.log(liquidity);
 
         // Renounce ownership + burn liquidity here
     }
