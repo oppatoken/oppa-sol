19074a51ffb2901a41580a11f6b6572839851781
Breech Alcres Paul Oracion <oracionbre@gmail.com>
2021-09-14 01:54:05 +0800

    (chore) testing supply

--- test/index.test.js
+++ test/index.test.js
@@ -1,9 +1,28 @@
-const { expect } = require('chai');
+const { expect } = require("chai");
 
-describe('EnrileFinance', function () {
-  it('it should deploy EnrileFinance', async function () {
-    const EnrileFinance = await ethers.getContractFactory('EnrileFinance');
-    const enrileFinance = await EnrileFinance.deploy();
-    const deployed = await enrileFinance.deployed();
+describe("Oppa Token", function () {
+  let OppaToken;
+  let owner;
+  let oppaDeployed;
+
+  this.beforeEach(async () => {
+    OppaToken = await ethers.getContractFactory("Oppa", {
+      libraries: {
+        IterableMapping: "0x71784F9F113ADC6B19F84fcE6035ebA2FbD4d4B4",
+      },
+    });
+
+    owner = await (await ethers.getSigners())[0];
+
+    const oppaToken = await OppaToken.deploy();
+    oppaDeployed = await oppaToken.deployed();
   });
+
+  it("Owner balance should be token supply", async function () {
+    expect(await oppaDeployed.balanceOf(owner.address)).to.equal(
+      await oppaDeployed.totalSupply()
+    );
+  });
+
+  // @TODO: test rewards
 });
